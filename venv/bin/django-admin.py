79451000a5425570672e23123519d5bed8832478
Valentin BUS <valentinoiho@gmail.com>
2019-10-07 10:40:03 +0200

    Remove venv folder

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/valentinbus/Documents/repo/openclassrooms/p8/venv/bin/python3.7
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
