7dc67f9df9fb4161712c489d62ba25f879495ca3
valentinb <valentin.bus@d2si.io>
2019-12-15 14:11:50 +0100

    [backend] Change form field name

--- pur_beurre/connexion/forms.py
+++ pur_beurre/connexion/forms.py
@@ -6,7 +6,7 @@ class ConnexionForm(forms.Form):
     password = forms.CharField(label="Password", widget=forms.PasswordInput(attrs={'class' : 'input-form-django'}))
 
 class RegistrationForm(forms.Form):
-    username = forms.CharField(max_length=30, widget=forms.TextInput(attrs={'class' : 'input-form-django'}))
-    email = forms.EmailField(label="E-mail", max_length=100, widget=forms.TextInput(attrs={'class' : 'input-form-django'}))
-    password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'input-form-django'}))
-    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={'class' : 'input-form-django'}))
+    username = forms.CharField(label="Nom d'utilisateur", max_length=30, widget=forms.TextInput(attrs={'class' : 'input-form-django'}))
+    email = forms.EmailField(label="Adresse mail", max_length=100, widget=forms.TextInput(attrs={'class' : 'input-form-django'}))
+    password = forms.CharField(label="Mot de passe", widget=forms.PasswordInput(attrs={'class' : 'input-form-django'}))
+    confirm_password = forms.CharField(label="Confirmez votre mot de passe", widget=forms.PasswordInput(attrs={'class' : 'input-form-django'}))
