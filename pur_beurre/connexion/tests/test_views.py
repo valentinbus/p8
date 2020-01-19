69d19baffec8fa2f4c322404d1d0266857d11028
valentinb <valentin.bus@d2si.io>
2020-01-19 18:45:53 +0100

    [unittest] improve coverage to 88% connexion-app

--- pur_beurre/connexion/tests/test_views.py
+++ pur_beurre/connexion/tests/test_views.py
@@ -1,12 +1,60 @@
-# import unittest
-# from django.test import TestCase, Client
-# from django.urls import reverse
-# from django.contrib.auth.models import User
+import unittest
+import logging
+
+from pprint import pformat
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.core.paginator import Paginator
+from openfoodfact.models import Product, Save
+from django.contrib.postgres.search import SearchQuery, SearchRank, SearchVector
 
 
-# class TestViews(TestCase):
-#     def connexion_pass(self):
-#         client = Client()
-#         response = client.post(reverse('connexion'))
+logging.basicConfig(level=logging.DEBUG)
+client = Client()
 
-#         self.assertEquals(response.status_code)
+class ViewsTest(TestCase):
+    def test_connexion(self):
+        User.objects.create_user(
+            username='valentin',
+            email='a@a.com',
+            password='valentin'
+        )
+
+        response = client.post(
+            reverse('connexion'),
+            {
+                'username': 'valentin',
+                'password': 'valentin'
+            } 
+        )
+
+        self.assertEquals(response.status_code, 200)
+
+    def test_registration(self):
+        response = client.post(
+            reverse('registration'),
+            {
+                'username': 'test',
+                'email': 'test@test.com',
+                'password': 'password',
+                'confirm_password': 'password'
+            }
+        )
+
+        self.assertEquals(response.status_code, 200)
+
+    def test_deconnexion(self):
+        user = User.objects.create_user(
+            username='valentin',
+            email='a@a.com',
+            password='valentin'
+        )
+
+        client.login(username='valentin', password='valentin')
+
+        response = client.get(
+            reverse('deconnexion')
+        )
+
+        self.assertEquals(response.status_code, 200)
