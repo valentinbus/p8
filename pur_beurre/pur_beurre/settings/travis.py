0186c9486c9c00ae63a119a58842416150a3e2d8
valentinbus <valentinoiho@gmail.com>
2020-03-28 10:24:31 +0000

    [ADD] add travis config & gitignore || requirements modifications

---
+++ pur_beurre/pur_beurre/settings/travis.py
@@ -0,0 +1,12 @@
+from . import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': 'postgres',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',
+    }
+}
